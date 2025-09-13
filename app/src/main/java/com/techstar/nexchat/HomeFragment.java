cb4e78d4f1795d334d88bd04b9d3e3f541e614cc
zhangzm0 <zhangzhaoman2023@outlook.com>
2025-09-13 22:33:59 +0800

    first commit

---
+++ app/src/main/java/com/techstar/nexchat/HomeFragment.java
@@ -0,0 +1,12 @@
+package com.techstar.nexchat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.*;
+
+public class HomeFragment extends Fragment {
+    public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        return i.inflate(R.layout.frag_home, c, false);
+    }
+}
+
---
+++ app/src/main/java/com/techstar/nexchat/MainActivity.java
@@ -0,0 +1,44 @@
+package com.techstar.nexchat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        // 黑色导航栏
+        getWindow().setNavigationBarColor(0xff121212);
+
+        ViewPager vp = new ViewPager(this);
+        vp.setId(R.id.pager);
+        vp.setAdapter(new NexPager(getSupportFragmentManager()));
+        vp.setCurrentItem(1, false);          // 默认聊天页
+        // 轻量切换动画（8 dp 视差 + 10 % 淡入）
+        vp.setPageTransformer(false, new ViewPager.PageTransformer() {
+                public void transformPage(View v, float pos) {
+                    v.setTranslationX(pos < 0 ? 0 : -pos * 8);
+                    v.setAlpha(1 - Math.abs(pos) * 0.1f);
+                }
+            });
+        setContentView(vp);
+    }
+
+    private static class NexPager extends FragmentPagerAdapter {
+        NexPager(FragmentManager fm) { super(fm); }
+        @Override public int getCount() { return 3; }
+        @Override public Fragment getItem(int p) {
+            switch (p) {
+                case 0: return new HomeFragment();
+                case 1: return new ChatFragment();
+                default: return new InputFragment();
+            }
+        }
+    }
+}
+
