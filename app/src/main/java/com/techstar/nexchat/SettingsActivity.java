69a91f0234bfe32569b3d671be1f09ee39278245
zhangzm0 <zhangzhaoman2023@outlook.com>
2025-09-23 18:54:51 +0800

    first commit

---
+++ app/src/main/java/com/techstar/nexchat/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.techstar.nexchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        
+    }
+    
+}
