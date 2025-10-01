02bc8bcfac846f43dc88fbf26cb85a525481f935
zhangzm0 <zhangzhaoman2023@outlook.com>
2025-10-01 13:11:25 +0800

    Revert "first commit"
    This reverts commit bb1fdb8b07de9925fe0045a68d72f06f933c5ca1.

--- app/src/main/java/com/techstar/nexchat/ChatFragment.java
+++ app/src/main/java/com/techstar/nexchat/ChatFragment.java
@@ -535,7 +535,7 @@ public class ChatFragment extends Fragment {
 
 		// 检查是否正在更新
 		if (isUpdating) {
-			Log.e("ChatFragment", "Already updating, skip new message");
+			log.e("ChatFragment", "Already updating, skip new message");
 			return;
 		}
 
@@ -664,7 +664,7 @@ public class ChatFragment extends Fragment {
 			}
 
 		} catch (final Exception e) {
-			Log.e("ChatFragment", "Stream response processing failed");
+			log.e("ChatFragment", "Stream response processing failed", e);
 			if (getActivity() != null) {
 				getActivity().runOnUiThread(new Runnable() {
 						@Override
@@ -694,13 +694,13 @@ public class ChatFragment extends Fragment {
 											try {
 												recyclerView.smoothScrollToPosition(messages.size() - 1);
 											} catch (Exception e) {
-												Log.e("ChatFragment", "Scroll failed");
+												log.e("ChatFragment", "Scroll failed", e);
 											}
 										}
 									});
 							}
 						} catch (Exception e) {
-							Log.e("ChatFragment", "Safe scroll failed");
+							log.e("ChatFragment", "Safe scroll failed", e);
 						}
 					}
 				});
@@ -709,7 +709,7 @@ public class ChatFragment extends Fragment {
 
 	// 修复错误处理方法
 	private void handleError(final String error) {
-		Log.e("ChatFragment", "Error: " + error);
+		log.e("ChatFragment", "Error: " + error);
 
 		if (getActivity() != null) {
 			getActivity().runOnUiThread(new Runnable() {
